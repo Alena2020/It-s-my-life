fe0af69e2001684968584f4c95308172f09b614f
Alena <elenila_skinef@mail.ru>
2022-08-26 20:47:11 +0300

    added local Storage for films

--- js/filmsList.js
+++ js/filmsList.js
@@ -5,18 +5,48 @@ const filmComment = document.getElementById('filmComment');
 const films = document.getElementById('filmsRow');
 const filmButton = document.getElementById('filmButton');
 
-filmButton.addEventListener('click', (event) => {
-  event.preventDefault();
+let filmsList = [];
+let film = {
+  title: '',
+  comment: ''
+};
 
+function loadLocalStorage() {
+    if (localStorage.getItem("films")) {
+        filmsList = JSON.parse(localStorage.getItem("films")) || [];
+        render();
+    }
+}
+
+loadLocalStorage();
+
+function updateLocalStorage() {
+    localStorage.setItem("films", JSON.stringify(filmsList));
+}
+
+function addFilm(newFilm) {
   if (filmTitle.value !== '' && filmComment.value !== '') {
-    let filmTemplate = '';
+      film = {...newFilm}; 
+      filmsList.push(film);
+      render();
+      updateLocalStorage();
+  }
+}
 
-    filmTemplate = `
+function render() {
+  let filmTemplate = '';
+
+  filmsList.forEach(function(item, index) {
+
+    filmTitle.value = '';        
+    filmComment.value = '';
+
+    filmTemplate += `
         <div class="col-lg-4 col-md-6 py-3">
           <div class="card h-100">
             <div class="card-body">
-              <h4 class="card-title">${filmTitle.value}</h4>
-              <p>${filmComment.value}</p>
+              <h4 class="card-title">${item.title}</h4>
+              <p>${item.comment}</p>
               <div class="form-check">
                 <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
                 <label class="form-check-label" for="flexCheckDefault">Film viewed</label>
@@ -25,13 +55,15 @@ filmButton.addEventListener('click', (event) => {
           </div>
         </div>
     `;
+  });
 
-    films.innerHTML += filmTemplate;
+    films.innerHTML = filmTemplate;  
+}
 
-    // Clear input
-    filmTitle.value = '';        
-    filmComment.value = '';
-    
-  }
+
+filmButton.addEventListener('click', (event) => {
+  event.preventDefault();
+
+  addFilm({title: filmTitle.value, comment: filmComment.value});
 });
 
